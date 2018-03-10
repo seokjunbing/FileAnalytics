42d76cae228cc5497e0dd1133df25dbd2ed57dcc
juantorres <juan.c.torres.ramirez.17@dartmouth.edu>
2018-03-09 23:02:25 -0500

    Various improvements and bug fixes

--- src/analysis.py
+++ src/analysis.py
@@ -13,28 +13,35 @@ IS_DEAD
 """
 
 import time
-import csv
 from typing import List
+from os import path
 
 import matplotlib.pyplot as plt
 import pandas as pd
 
 START_TIME = time.time()
-# FILENAME = '../data/test_out_copy.tsv'
-# FILENAME = '../data/test_out_FINAL.tsv'
-FILENAME = '../data/test_jun.tsv'
+DATASET = 'jun'  # 'juan' or 'jun'
+FILENAME = '../data/test_%s.tsv' % DATASET
 
+# we will split our dataset into smaller datasets to generate descriptive statistics
 BUCKET_BOUNDARIES = [0, 1, 10, 100, 500, 1000, 10000, 100000]
-BUCKET_LABELS = ['%s-%s' % (BUCKET_BOUNDARIES[i - 1], BUCKET_BOUNDARIES[i]) for i in range(1, len(BUCKET_BOUNDARIES))]
+BUCKET_LABELS = ['0', '1-10', '11-100', '101-500', '501-1000', '1001-10000', '10001-100000']
 
-DF_COLUMNS = ['filetype', 'extension', 'blocks', 'birthtime', 'alive_for_periods', 'is_dead']
-EXTENSION_COLUMN = DF_COLUMNS[1]
-BIRTHTIME_COLUMN = DF_COLUMNS[3]
-LIFETIME_COLUMN = DF_COLUMNS[4]
-IS_DEAD_COLUMN = DF_COLUMNS[5]
+DATAFRAME_COLUMNS = ['filetype', 'extension', 'blocks', 'birthtime', 'alive_for_periods', 'is_dead']
+EXTENSION_COL = DATAFRAME_COLUMNS[1]
+BIRTHTIME_COL = DATAFRAME_COLUMNS[3]
+ALIVE_FOR_COL = DATAFRAME_COLUMNS[4]
+IS_DEAD_COL = DATAFRAME_COLUMNS[5]
+
 # not included in the list because this one is not present in the original .tsv and the primary purpose of the list is
-# to name the columns in that .tsv
-DF_BLOCKSIZE_CATEGORY = 'blocksize_category'
+# to name the columns in that .tsv. In other words, these are columns we generate for later
+# processing
+BLOCKSIZE_CATEGORY_COL = 'blocksize_category'
+TOTAL_LIFETIME_COL = 'lifetime'
+TOTAL_LIFETIME_DAYS_COL = 'lifetime_days'
+ALIVE_OBSERVED_MINUTES_COL = 'alive_for_minutes'
+
+PREPROCESSED_SUFFIX = '_preprocessed.tsv'
 
 
 # def create_block_vs_lifetime_table(df):
@@ -57,11 +64,6 @@ DF_BLOCKSIZE_CATEGORY = 'blocksize_category'
 # if file_in_range(is_alive, birth_time, periods_alive):
 #     pass
 
-
-def file_in_range(is_alive, birth_time, periods_alive):
-    return birth_time > START_TIME and not is_alive
-
-
 def get_blocksize_dataframes(df: pd.DataFrame, state: str, column='blocksize_category'):
     """
     :param df: data frame
@@ -73,7 +75,7 @@ def get_blocksize_dataframes(df: pd.DataFrame, state: str, column='blocksize_cat
 
     if state == state_read:
         for i in range(len(BUCKET_LABELS)):
-            df_filtered = df[df['%s' % DF_BLOCKSIZE_CATEGORY] == BUCKET_LABELS[i]]
+            df_filtered = df[df['%s' % BLOCKSIZE_CATEGORY_COL] == BUCKET_LABELS[i]]
             df_blocksize_list.append(df_filtered)
             filename = '../data/block%s.tsv' % BUCKET_LABELS[i]
             df_filtered.to_csv(filename, sep='\t')
@@ -88,7 +90,32 @@ def get_blocksize_dataframes(df: pd.DataFrame, state: str, column='blocksize_cat
     return df_blocksize_list
 
 
-def get_dataframe(state):
+def preprocess_data(end_time):
+    index_label = 'num'
+    is_dead_index = 5
+
+    df = pd.read_csv(FILENAME, sep='\t', header=None, names=DATAFRAME_COLUMNS, dtype={is_dead_index: bool})
+
+    # add a column with the category (by blocksize)
+    cuts = pd.cut(df['blocks'], bins=BUCKET_BOUNDARIES, labels=BUCKET_LABELS)
+    df['blocksize_category'] = cuts
+
+    # add a column with the lifetime of the file
+    df[TOTAL_LIFETIME_COL] = df[BIRTHTIME_COL]
+
+    # using loc because assigning a new column to the dataframe as we did creates a copy of the data.
+    df[TOTAL_LIFETIME_COL] = df.loc[0:, TOTAL_LIFETIME_COL].apply(lambda birthtime: end_time - birthtime)
+    df[TOTAL_LIFETIME_DAYS_COL] = df[TOTAL_LIFETIME_COL].apply(lambda lifetime: to_days(lifetime))
+
+    # convert periods monitored to minutes: 24 hours divided by how many periods were seen by the script
+    minutes_per_period = (24 * 60) / df[ALIVE_FOR_COL].max()
+    df[ALIVE_OBSERVED_MINUTES_COL] = df[ALIVE_FOR_COL].apply(lambda periods_alive: periods_alive * minutes_per_period)
+
+    df.to_csv('%s%s' % (FILENAME, PREPROCESSED_SUFFIX), sep='\t', index_label=index_label)
+    return df
+
+
+def get_dataframe(state, filename, end_time=None):
     """
     First read of the raw data.
         Do light processing to add a blocksize category (1-10, 11-100, etc.)
@@ -96,52 +123,27 @@ def get_dataframe(state):
         Read the tables from the .tsv file in disk
     """
     index_label = 'num'
-    is_dead_index = 5
 
-    if state == state_read:
-        # initial creation of the table does not contain column names. The pandas library works a lot better
-        # with column names, so add them and then save the file to disk
-        df = pd.read_csv(FILENAME, sep='\t', header=None, names=DF_COLUMNS, dtype={is_dead_index: bool})
+    # headers already formatted and included
+    df = pd.read_csv(filename, sep='\t')
+    # Some files have characters that are written as newlines in the .csv file.
+    # We originally thought this would happen only at the end of filenames and are cleaning those characters
+    # there, but it turns out this also happens at the beginning of filenames. Data collection makes it unfeasible
+    # to fix our data collection at this stage, so this is a patch.
+    # The weird characters make the pandas library think the row index is not numeric because the newline
+    # results in one of the lines start with a string. This dirty solution consists of ignoring the lines
+    # that do not start with an int. The previous line containing the filename that had a new line in
+    # the middle becomes unusable (the columns become NaN after the numeric conversion, but this is fine for now,
+    # since we are only looking at statistics for now and the NaN get ignored for those.
 
-        # TEST
-        # df.index = df[pd.to_numeric(df.index, errors='coerce').notnull()]
-        # TEST
+    # df[IS_DEAD_COLUMN] = df[IS_DEAD_COLUMN].astype(bool)
+    # print(df[IS_DEAD_COLUMN].dtype)
+    df2 = df[pd.to_numeric(df[index_label], errors='coerce').notnull()]
+    df2.set_index(index_label)
+    df2 = df2.dropna(subset=['birthtime', 'alive_for_periods'])
+    df2['alive_for_periods'] = df2['alive_for_periods'].apply(lambda x: pd.to_numeric(x, errors='coerce'))
 
-        # Create one table for each blocksize category, i.e., separate the files that fall in the 1-10 blocks
-        # category in a separate table and so on. Helps when calculating descriptive statistics
-        cuts: pd.Series = pd.cut(
-            df['blocks'],
-            bins=BUCKET_BOUNDARIES,
-            labels=BUCKET_LABELS
-        )
-        df['blocksize_category'] = cuts  # add a new column with the blocksize category of each file
-        df.to_csv('%s_processed.tsv' % FILENAME, sep='\t', index_label=index_label)  # save to disk
-        return df
-    elif state_process == state_process:
-        # headers already formatted and included
-        df = pd.read_csv('%s_processed.tsv' % FILENAME, sep='\t')
-        # Some files have characters that are written as newlines in the .csv file.
-        # We originally thought this would happen only at the end of filenames and are cleaning those characters
-        # there, but it turns out this also happens at the beginning of filenames. Data collection makes it unfeasible
-        # to fix our data collection at this stage, so this is a patch.
-        # The weird characters make the pandas library think the row index is not numeric because the newline
-        # results in one of the lines start with a string. This dirty solution consists of ignoring the lines
-        # that do not start with an int. The previous line containing the filename that had a new line in
-        # the middle becomes unusable (the columns become NaN after the numeric conversion, but this is fine for now,
-        # since we are only looking at statistics for now and the NaN get ignored for those.
-
-        # df[IS_DEAD_COLUMN] = df[IS_DEAD_COLUMN].astype(bool)
-        # print(df[IS_DEAD_COLUMN].dtype)
-        df2 = df[pd.to_numeric(df[index_label], errors='coerce').notnull()]
-        df2.set_index(index_label)
-        df2 = df2.dropna(subset=['birthtime', 'alive_for_periods'])
-        df2['alive_for_periods'] = df2['alive_for_periods'].apply(lambda x: pd.to_numeric(x, errors='coerce'))
-
-        # print(df2[IS_DEAD_COLUMN].dtype)
-        # df2.to_csv('../data/test_test.tsv', sep='\t', index=0)
-        # print(df2[IS_DEAD_COLUMN].dtype)
-        return df2
-        # return df2
+    return df2
 
 
 def mean(df: pd.DataFrame, column):
@@ -149,7 +151,7 @@ def mean(df: pd.DataFrame, column):
 
 
 def mean_lifetime(df: pd.DataFrame):
-    return mean(df, LIFETIME_COLUMN)
+    return mean(df, ALIVE_FOR_COL)
 
 
 def stddev(df: pd.DataFrame, column):
@@ -162,16 +164,64 @@ STATE_ALL = {state_read, state_process}
 STATE_CURRENT = state_process
 
 
-def blocksize_stats(df):
-    df_blocksize_list: List[pd.DataFrame] = get_blocksize_dataframes(df, state=STATE_CURRENT)
+def blocksize_stats(df, label, verbose_mode):
+    print('--BLOCKSIZE STATS--')
+    print('-%s-' % label)
 
-    for i in range(len(df_blocksize_list)):
-        print('Block size: %s' % BUCKET_LABELS[i])
-        df_filtered = df_blocksize_list[i]
-        print('count: %d' % len(df_filtered))
-        print('stddev for lifetime: ' + str(stddev(df_filtered, LIFETIME_COLUMN)))
-        print('mean for lifetime: ' + str(mean(df_filtered, LIFETIME_COLUMN)))
-        print('\n')
+    df_new = pd.DataFrame()
+    df_new['category'] = BUCKET_LABELS
+    mean_lifetime_list = []
+    std_lifetime_list = []
+    mean_alive_list = []
+    std_alive_list = []
+    count_list = []
+
+    for i in range(len(BUCKET_LABELS)):
+        df_filtered = df[df[BLOCKSIZE_CATEGORY_COL] == BUCKET_LABELS[i]]
+        mean_alive_list.append(mean(df_filtered, ALIVE_FOR_COL))
+        std_alive_list.append(stddev(df_filtered, ALIVE_FOR_COL))
+        mean_lifetime_list.append(to_days(mean(df_filtered, TOTAL_LIFETIME_COL)))
+        std_lifetime_list.append(to_days(stddev(df_filtered, TOTAL_LIFETIME_COL)))
+        count_list.append(len(df_filtered))
+
+        if verbose_mode:
+            print('Block size: %s' % BUCKET_LABELS[i])
+            print('count: %d' % len(df_filtered))
+
+            print('mean for alive periods monitored (periods): ' + str(mean(df_filtered, ALIVE_FOR_COL)))
+            print('stddev for alive periods monitored (periods): ' + str(stddev(df_filtered, ALIVE_FOR_COL)))
+            print(
+                '\tmin: %s, max: %s' % (
+                    to_days(df_filtered[ALIVE_FOR_COL].min()), to_days(df_filtered[ALIVE_FOR_COL].max())
+                )
+            )
+
+            print('mean for lifetime approximation (days): ' + str(to_days(mean(df_filtered, TOTAL_LIFETIME_COL))))
+            print(
+                'stddev for lifetime approximation (days): ' + str(to_days(stddev(df_filtered, TOTAL_LIFETIME_COL))))
+            print(
+                '\tmin: %s, max: %s' % (
+                    to_days(df_filtered[TOTAL_LIFETIME_COL].min()), to_days(df_filtered[TOTAL_LIFETIME_COL].max())
+                )
+            )
+            print('\n')
+
+    df_new['alive_mean_periods'] = mean_alive_list
+    df_new['alive_std_periods'] = std_alive_list
+    df_new['lifetime_mean_days'] = mean_lifetime_list
+    df_new['lifetime_std_days'] = std_lifetime_list
+    df_new['count'] = count_list
+    print('%s\n' % df_new)
+    df_new.to_csv('../data/blocksize_stats_%s_%s.csv' % (DATASET, label), index=False, )
+
+
+def to_days(seconds):
+    """
+    String for number of days in
+    :param seconds:
+    :return:
+    """
+    return seconds / 3600 / 24
 
 
 def filetype_stats(df):
@@ -197,12 +247,12 @@ def filetype_stats(df):
         extension_group = filetypes_all[i]
         # df_extension = df[df[EXTENSION_COLUMN].isin(extension_group)]
         # for extension_group in filetypes_all:
-        df_extension = df[df[EXTENSION_COLUMN].isin(extension_group)]
+        df_extension = df[df[EXTENSION_COL].isin(extension_group)]
         print('%s files: %s' % (filetype_names[i], str(extension_group)))
         print('count: %d' % len(df_extension))
-        t = [type(x) for x in df_extension[LIFETIME_COLUMN]]
-        print('stddev for lifetime: ' + str(stddev(df_extension, LIFETIME_COLUMN)))
-        print('mean for lifetime: ' + str(mean(df_extension, LIFETIME_COLUMN)))
+        t = [type(x) for x in df_extension[ALIVE_FOR_COL]]
+        print('stddev for lifetime: ' + str(stddev(df_extension, ALIVE_FOR_COL)))
+        print('mean for lifetime: ' + str(mean(df_extension, ALIVE_FOR_COL)))
         print('\n')
 
 
@@ -213,7 +263,7 @@ def filetype_histogram(df: pd.DataFrame):
     print('--HISTOGRAM--')
     print('COUNT: %s' % len(df))
     print(len(df))
-    dist = df[EXTENSION_COLUMN].value_counts(dropna=False).head(20)
+    dist = df[EXTENSION_COL].value_counts(dropna=False).head(20)
     print(dist)
     print('\n')
     plt.hist(dist)
@@ -221,35 +271,52 @@ def filetype_histogram(df: pd.DataFrame):
 
 
 if __name__ == '__main__':
-    start_time = 1520451300
 
-    if STATE_CURRENT == state_read:
-        df_all_files = get_dataframe(state_read)
-        # df_blocksize_list = get_blocksize_dataframes(df_all_files, state=STATE_CURRENT)
-        # for i in range(len(df_blocksize_list)):
-        #     print(i)
-        #     df_filtered = df_blocksize_list[i]
-        #     print('stddev for lifetime: ' + str(stddev(df_filtered, LIFETIME_COLUMN)))
-        #     print('mean for lifetime: ' + str(mean(df_filtered, LIFETIME_COLUMN)))
+    verbose = False
 
+    start_time = 1520451300  # 03/07/2018 2:30 PM
+    end_time = 1520550000  # 03/08/2018 06:00 PM
+    years_twoandahalf = int(365 * 2.5)  # for filtering old files
 
-    elif STATE_CURRENT == state_process:
-        df_all_files = get_dataframe(state_process)
-        df_dead_files = df_all_files[df_all_files[IS_DEAD_COLUMN] == True]
-        df_alive_and_dead_files = df_dead_files[df_dead_files[BIRTHTIME_COLUMN] > start_time]
+    preprocessed_fname = FILENAME + PREPROCESSED_SUFFIX
+    if not path.isfile(preprocessed_fname):
+        # if STATE_CURRENT == state_read:
+        # df_all_files = get_dataframe(state_read, end_time)
+        preprocess_data(end_time)
 
-        # blocksize
-        blocksize_stats(df_all_files)
-        blocksize_stats(df_dead_files)
-        # and filetype
-        filetype_stats(df_all_files)
-        filetype_stats(df_dead_files)
-        # most common filetypes
-        filetype_histogram(df_all_files)
-        filetype_histogram(df_dead_files)
-        print('ALIVE AND DEAD')
-        print(df_dead_files[BIRTHTIME_COLUMN].dtype)
-        filetype_histogram(df_alive_and_dead_files)
+    # elif STATE_CURRENT == state_process:
+    df_all = get_dataframe(state_process, preprocessed_fname)
+    df_all = df_all[df_all[TOTAL_LIFETIME_DAYS_COL] <= years_twoandahalf]
+    print('There are %d files in this dataset' % len(df_all))
+    # files deleted during the monitoring period
+    df_deleted = df_all[df_all[IS_DEAD_COL] == True]
+    # files created and deleted during the monitoring period
+    df_created_and_deleted = df_deleted[df_deleted[BIRTHTIME_COL] > start_time]
+    # the difference of the two previous sets, i.e., files created _before_ the monitoring period
+    # that died _during_ the monitoring period
+    df_not_created_and_deleted = df_deleted[~df_deleted.index.isin(df_created_and_deleted.index)]
 
-        print('Count of dead files: %d' % len(df_dead_files))
-        print('Count of alive and dead files: %d' % len(df_alive_and_dead_files))
+    # These don't work in a standalone python script, for some reason
+    # df_created_and_deleted.hist(column='alive_for_periods')
+    # plt.plot()
+
+    print(len(df_not_created_and_deleted))
+
+    # blocksize
+    blocksize_stats(df_all, 'ALL FILES', verbose)
+    blocksize_stats(df_deleted, 'FILES DELETED WHILE BEING MONITORED', verbose)
+    blocksize_stats(df_not_created_and_deleted, 'FILES CREATED BEFORE MONITORING, DELETED DURING MONITORING', verbose)
+    # # and filetype
+    # filetype_stats(df_all_files)
+    # filetype_stats(df_dead_files)
+    # # most common filetypes
+    # filetype_histogram(df_all_files)
+    # filetype_histogram(df_dead_files)
+    # filetype_histogram(df_alive_and_dead_files)
+
+    # print('ALIVE AND DEAD')
+    # print(df_dead_files[BIRTHTIME_COLUMN].dtype)
+
+    print('Count of deleted files: %d' % len(df_deleted))
+    print('Count of created and deleted files: %d' % len(df_created_and_deleted))
+    print('Count of pre-existing files that were deleted: %d' % len(df_not_created_and_deleted))
